2dca6bf55966840a330e1f962e56026489f0032d
Lennon <lennon_ximenes@hotmail.com>
2023-04-17 15:49:57 -0300

    go

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,17 @@
-/* Desenvolva sua lógica aqui ... */
+function musicGenre (array){
+    const listUl = document.querySelector(".container__musicGenre--Ul");
+    
+    array.forEach((button, index) => {
+        const listLi = document.createElement("li");
+        listLi.classList.add("licard");
+
+        const btn = document.createElement("button");
+        btn.classList.add("btnGenreMusic");
+        btn.innerText = button;
+        btn.dataset.musicGenre = Number(index);
+        listLi.appendChild(btn);
+
+        listUl.appendChild(listLi);
+    });
+}
+musicGenre(categories);
