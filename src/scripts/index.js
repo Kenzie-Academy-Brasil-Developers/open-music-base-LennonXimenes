5a9444d74b35fb07cd7fa81fd29e0783c85e1628
Lennon <lennon_ximenes@hotmail.com>
2023-04-18 11:58:05 -0300

    go

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -25,14 +25,72 @@ function setPrice (){
     value = Number(event.target.value);
         
     price.innerHTML  = `${value.toFixed(2)}`
+    
+        // const setPriceFilter = array.filter((priceFilter) =>{
+    //     return priceFilter.id >= range.value
+    // }) 
     })
 }
 setPrice();
 
-function albumsFound (array){
+function albumsFound (obj){
     const listUl = document.querySelector(".container__albumsFound--Ul");
 
-    array.forEach(insertedAlbum => {
-        
+    obj.forEach(insertedAlbum => {
+        const listLi = document.createElement("li");
+        listLi.classList.add("licardAlbums");
+
+         const divAlbum = document.createElement("div");
+         divAlbum.classList.add("divAlbum");
+
+         const divImg = document.createElement("div");
+         divImg.classList.add("divImg");
+
+         const imgAlbum = document.createElement("img");
+         imgAlbum.classList.add("imgAlbum");
+         imgAlbum.src = insertedAlbum.img;
+         divImg.appendChild(imgAlbum);
+
+        const divBandYear = document.createElement("div");
+        divBandYear.classList.add("divBandYear");
+
+         const bandAlbum = document.createElement("small");
+         bandAlbum.classList.add("bandAlbum");
+         bandAlbum.innerText = insertedAlbum.band;
+         divBandYear.appendChild(bandAlbum);
+
+         const yearAlbum = document.createElement("small");
+         yearAlbum.classList.add("yearAlbum");
+         yearAlbum.innerText = Number(insertedAlbum.year);
+         divBandYear.appendChild(yearAlbum);
+
+        const divTitle = document.createElement("div");
+        divTitle.classList.add("divTitle");
+
+         const titleAlbum = document.createElement("h3");
+         titleAlbum.classList.add("titleAlbum");
+         titleAlbum.innerText = insertedAlbum.title;
+         divTitle.appendChild(titleAlbum);
+
+         const divPriceBtn = document.createElement("div");
+         divPriceBtn.classList.add("divPriceBtn");
+
+         const priceAlbum = document.createElement("p");
+         priceAlbum.classList.add("priceAlbum");
+         priceAlbum.innerText = `R$ ${insertedAlbum.price.toFixed(2)}`;
+         divPriceBtn.appendChild(priceAlbum);
+
+         const btnAlbum = document.createElement("button");
+         btnAlbum.classList.add("btnAlbum");
+         btnAlbum.innerText = "Comprar";
+         divPriceBtn.appendChild(btnAlbum);
+
+         listLi.id = insertedAlbum.id
+         
+         divAlbum.append(divImg, divBandYear, divTitle, divPriceBtn);
+         
+         listLi.appendChild(divAlbum);
+         listUl.appendChild(listLi)
     });
 }
+albumsFound(products);
--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,1 +1,48 @@
-/* Desenvolva sua lógica aqui ... */
+function darkMode (){
+    const html = document.querySelector("html");
+    const darkButton = document.querySelector("mode__controller");
+
+    const modePreference = JSON.parse(localStorage.getItem("dark__mode"))
+
+    if (modePreference){
+        darkButton.innerText = "light mode"; /*por a foto img.src*/
+        html.classList.add("dark__mode");
+    } else {
+        darkButton.innerText = "dark mode"; /*por a foto*/
+        html.classList.remove("dark__mode");
+    }
+
+    darkButton.addEventListener("click", () => {
+        html.classList.toggle("dark__mode");
+    
+        if (html.classList.contains("dark__mode")){
+            darkButton.innerText = "light mode"
+            localStorage.setItem("dark__mode", true)
+        } else {
+            darkButton.innerText = "dark mode"
+            localStorage.setItem("dark__mode", false)
+        }
+    })
+}
+
+function render (array){
+    const seriesUl = document.querySelector("series__list");    
+    const animesUl = document.querySelector("series__list");    
+    const othersUl = document.querySelector("series__list");    
+
+    seriesUl.innerHTML = " ";
+    animesUl.innerHTML = " ";
+    othersUl.innerHTML = " ";
+
+    array.forEach(element => {
+        const card = createCard(element);
+        
+        if(element.type === "serie"){
+        seriesUl.appendChild(card);
+        } else if (CharacterData.type === "anime"){
+            animesUl.appendChild(card);
+        } else if (CharacterData.type === "other"){
+            othersUl.appendChild(card)
+        }
+    });
+}
