ea78a89ded965d39bef0f7969f7d09c73da30f9d
Lennon <lennon_ximenes@hotmail.com>
2023-04-19 17:17:06 -0300

    go

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,48 +1,27 @@
-function darkMode (){
+function darkMode (array){
     const html = document.querySelector("html");
-    const darkButton = document.querySelector("mode__controller");
-
-    const modePreference = JSON.parse(localStorage.getItem("dark__mode"))
-
-    if (modePreference){
-        darkButton.innerText = "light mode"; /*por a foto img.src*/
-        html.classList.add("dark__mode");
-    } else {
-        darkButton.innerText = "dark mode"; /*por a foto*/
-        html.classList.remove("dark__mode");
-    }
+    const darkButton = document.querySelector(".button__ilumination--mode");
 
     darkButton.addEventListener("click", () => {
         html.classList.toggle("dark__mode");
     
         if (html.classList.contains("dark__mode")){
-            darkButton.innerText = "light mode"
+            darkButton.src = "src/assets/img/moon.png"
             localStorage.setItem("dark__mode", true)
         } else {
-            darkButton.innerText = "dark mode"
+            darkButton.src = "src/assets/img/sun.png"
             localStorage.setItem("dark__mode", false)
         }
     })
+
+    const modePreference = JSON.parse(localStorage.getItem("dark__mode"))
+
+    if (modePreference){
+        darkButton.src = "src/assets/img/moon.png" /*por a foto img.src*/
+        html.classList.add("dark__mode");
+    } else {
+        darkButton.src = "src/assets/img.sun/png" /*por a foto*/
+        html.classList.remove("dark__mode");
+    }
 }
-
-function render (array){
-    const seriesUl = document.querySelector("series__list");    
-    const animesUl = document.querySelector("series__list");    
-    const othersUl = document.querySelector("series__list");    
-
-    seriesUl.innerHTML = " ";
-    animesUl.innerHTML = " ";
-    othersUl.innerHTML = " ";
-
-    array.forEach(element => {
-        const card = createCard(element);
-        
-        if(element.type === "serie"){
-        seriesUl.appendChild(card);
-        } else if (CharacterData.type === "anime"){
-            animesUl.appendChild(card);
-        } else if (CharacterData.type === "other"){
-            othersUl.appendChild(card)
-        }
-    });
-}
+darkMode()
