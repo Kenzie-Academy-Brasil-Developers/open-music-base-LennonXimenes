6558fbb674f7b6d06d5fef26c769cd2aeeeed4f9
Lennon <lennon_ximenes@hotmail.com>
2023-04-20 11:24:21 -0300

    pronto

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -3,26 +3,25 @@ function darkMode (array){
     const darkButton = document.querySelector(".button__ilumination--mode");
     darkButton.classList.add("darkButton");
 
-
     darkButton.addEventListener("click", () => {
         html.classList.toggle("dark__mode");
     
         if (html.classList.contains("dark__mode")){
-            darkButton.src = "src/assets/img/moon.png"
-            localStorage.setItem("dark__mode", true)
+            darkButton.src = "../src/assets/img/sun.png";
+            localStorage.setItem("dark__mode", true);
         } else {
-            darkButton.src = "src/assets/img/sun.png"
-            localStorage.setItem("dark__mode", false)
+            darkButton.src = "../src/assets/img/moon.png";
+            localStorage.setItem("dark__mode", false);
         }
     })
 
-    const modePreference = JSON.parse(localStorage.getItem("dark__mode"))
+    const modePreference = JSON.parse(localStorage.getItem("dark__mode"));
 
     if (modePreference){
-        darkButton.src = "src/assets/img/moon.png"
+        darkButton.src = "../src/assets/img/sun.png";
         html.classList.add("dark__mode");
     } else {
-        darkButton.src = "src/assets/img/sun.png"
+        darkButton.src = "../src/assets/img/moon.png";
         html.classList.remove("dark__mode");
     }
 }
